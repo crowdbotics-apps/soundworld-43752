75cd65bdf925d898087a28a74681ff4ba7190ad2
Crowdbotics <team@crowdbotics.com>
2023-10-04 08:15:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "soundworld-43752",
+  url: "https://soundworld-43752.botics.co",
+  api: "https://soundworld-43752.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
